8c710a407fa83e1ed20b2d349252e493aed30f14
Oleg Kudimov <ovkudimov@gmail.com>
2019-07-02 12:55:29 +0300

    added assert to test_items and make some changes

--- test_items.py
+++ test_items.py
@@ -1,5 +1,8 @@
+import time
+
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_guest_should_see_login_link(browser):
+def test_should_be_add_button_in_different_language(browser):
     browser.get(link)
-    browser.find_element_by_css_selector("#add_to_basket_form > button")
+    time.sleep(30)
+    assert browser.find_element_by_css_selector("#add_to_basket_form > button"), "Error, the button doesn't find"
