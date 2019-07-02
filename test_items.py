f9c79b5e94ae36a3c6ec428c6875e647050601c3
Oleg Kudimov <ovkudimov@gmail.com>
2019-07-02 12:47:50 +0300

    added two working files

---
+++ test_items.py
@@ -0,0 +1,5 @@
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_guest_should_see_login_link(browser):
+    browser.get(link)
+    browser.find_element_by_css_selector("#add_to_basket_form > button")
